5338956d7eba456dc38f1f8f1a96763f74c130e5
ZhalgasB <zhalgas.bissenbai@narxoz.kz>
2026-05-09 18:32:59 +0500

    Iterator completed and also new implementation of QuestIterator

--- src/com/narxoz/rpg/quest/ReverseQuestIterator.java
+++ src/com/narxoz/rpg/quest/ReverseQuestIterator.java
@@ -2,9 +2,7 @@ package com.narxoz.rpg.quest;
 
 import java.util.List;
 
-/**
- * Traverses quests from newest arrival back to oldest arrival.
- */
+
 public class ReverseQuestIterator implements QuestIterator {
 
     private final List<Quest> snapshot;
@@ -17,13 +15,13 @@ public class ReverseQuestIterator implements QuestIterator {
 
     @Override
     public boolean hasNext() {
-        // TODO: return true while the cursor still points at an unread quest.
-        return false;
+        return cursor >=0;
     }
 
     @Override
     public Quest next() {
-        // TODO: return the current quest and move the cursor backward.
-        return null;
+        if (!hasNext()) {
+            throw new java.util.NoSuchElementException("No more quests");
+        }return snapshot.get(cursor--);
     }
 }
